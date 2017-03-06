5c9007fab0356de6b373d673549a82cd14672608
Erik Dubois <erikdubois@users.noreply.github.com>
2017-03-06 06:33:13 +0100

    Update install-elementary-tweaks-v1.sh

--- install-elementary-tweaks-v1.sh
+++ install-elementary-tweaks-v1.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
 sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
 sudo apt-get update
 sudo apt-get install elementary-tweaks
